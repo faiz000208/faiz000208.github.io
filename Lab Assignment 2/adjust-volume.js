ef8b694cb8f8501e2fff2d25804f1c1e73c13116
faiz000208 <faiz820508@gmail.com>
2021-12-06 09:25:07 +0800

    Apply more Js, CSS, Html

---
+++ Lab Assignment 2/adjust-volume.js
@@ -0,0 +1,13 @@
+
+        var x = document.getElementById("myMusic");
+
+        function setHalfVolume() { 
+          x.volume = 0.2;
+          alert("The volume is 15% which is Low !!");
+        }
+
+        function setFullVolume() { 
+          x.volume = 1.0;
+          alert("The volume is 100% which is Full Volume !!");
+        }
+       
